59379d2ddbc9a427060eecf0bfbca4c280d4a46e
Thailan Gouvea <thailangm@gmail.com>
2023-08-23 18:09:34 +0300

    Compare 'computerChoice' values
    It's been added a chain of ternary conditional operators in order to
    attribute string values to 'computerChoice'.
    This will be, later, compared to user's input value.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 10) + 1;
+    (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "Rock" :
+    (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "Paper" :
+    computerChoice = "Scissors";
     console.log(computerChoice);
 }
