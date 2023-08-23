341985152c8b8b945a7cf8862af48277d3a808a3
Thailan Gouvea <thailangm@gmail.com>
2023-08-23 18:23:00 +0300

    Fix: replace 'console.log'
    It's been removed back from right below the variable declaration(placed
    there for testing purposes).
    Furthermore, 'console.log' on the original spot has been now commented
    back out.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 9) + 1;
-    console.log(computerChoice);
     (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "Rock" :
     (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "Paper" :
     computerChoice = "Scissors";
-    //console.log(computerChoice);
+    console.log(computerChoice);
 }
 getComputerChoice();
