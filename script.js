1f77fa1502848de28d74d2cf4dfe785232105fb8
Thailan Gouvea <thailangm@gmail.com>
2023-08-24 10:07:29 +0300

    Fix: change player's choice first letters Change has been made to match the previous change that had been made in the 'getComputerChoice' function. This one is also in order to make the comparisons case-insensitive.

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ const computerWeapon = getComputerChoice();
 let game = (player, computer) => {
     if(player == computer) {
         console.log("It's a tie!");
-    } else if(player == 'Rock') {
+    } else if(player == 'rock') {
         if(computer == 'paper'){
             console.log("You lose! Paper beats rock.");
         } else if(computer == 'scissors') {
             console.log("You won! Rock beats scissors.");
         }
-    } else if(player == 'Paper') {
+    } else if(player == 'paper') {
         if(computer == 'rock') {
             console.log("You won! Paper beats rock.");
         } else if (computer == "scissors") {
             console.log("You lose! Scissors beats paper.");
         }
-    } else if (player == 'Scissors') {
+    } else if (player == 'scissors') {
         if(computer == 'rock') {
             console.log("You lose! Rock beats Scissors");
         } else if (computer == "paper") {
