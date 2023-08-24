a36a25bf78511148916e7a5818b2581b1c4a5a00
Thailan Gouvea <thailangm@gmail.com>
2023-08-24 10:00:05 +0300

    Fix: replace 'computerChoice' first letters
    It's been changed to make easier to create a case-insensitive
    comparison between user's and comoputer's choices.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 9) + 1;
-    (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "Rock" :
-    (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "Paper" :
-    computerChoice = "Scissors";
+    (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "rock" :
+    (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "paper" :
+    computerChoice = "scissors";
     return computerChoice;
 }
 
