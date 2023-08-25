2aa619d60db534863198160aa11d5bc586bfb518
Thailan Gouvea <thailangm@gmail.com>
2023-08-25 19:32:12 +0300

    Increment scoreboard variables
    Scoreboard values additions have been added to, whenever the
    condition has been met, the respective actor gets 1 point summed up to
    its score.

--- script.js
+++ script.js
@@ -13,23 +13,30 @@ let computerScore = 0;
 let game = (player, computer) => {
     if(player == computer) {
         console.log("It's a tie!");
+        tieCount++;
     } else if(player == 'rock') {
         if(computer == 'paper'){
             console.log("You lose! Paper beats rock.");
+            computerScore++;
         } else if(computer == 'scissors') {
             console.log("You won! Rock beats scissors.");
+            userScore++
         }
     } else if(player == 'paper') {
         if(computer == 'rock') {
             console.log("You won! Paper beats rock.");
+            userScore++;
         } else if (computer == "scissors") {
             console.log("You lose! Scissors beats paper.");
+            computerScore++;
         }
     } else if (player == 'scissors') {
         if(computer == 'rock') {
             console.log("You lose! Rock beats Scissors");
+            computerScore++;
         } else if (computer == "paper") {
             console.log("You won! Scissors beats paper.")
+            userScore++;
         }
     } 
 }
