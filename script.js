3606683ce1428b0c5f7fdc717b0000ca51c7082e
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-05-14 20:02:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
-function getComputerChoice() {
-    /*let computerChoice = Math.floor(Math.random() * 9) + 1;
-    (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "rock" :
-    (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "paper" :
-    computerChoice = "scissors";
-    return computerChoice;*/
-    let computerChoice = "rock";
-    return computerChoice;
-}
-
 let tieCount = 0;
 let userScore = 0;
 let computerScore = 0;
 
+const paper = 'paper';
+const rock = 'rock';
+const scrs = 'scissors';
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 9) + 1;
+(computerChoice >= 1 && computerChoice <= 3) ? computerChoice = rock : (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = paper : computerChoice = scrs;
+}
+
 let game = (player, computer) => {
+  
+const message = document.getElementById("message");
+  
     if(player == computer) {
-        const para = document.createElement("p");
-        para.textContent = "It's a tie";
-        document.body.appendChild(para);
+        message.appendChild("It's a tie!");
         tieCount++;
-    } else if(player == 'rock') {
-        if(computer == 'paper'){
-            console.log("<p>You lose! Paper beats rock.<p>");
+    } else if(player == rock) {
+        if(computer == paper){
+            message.appendChild("You lose! Paper beats rock.");
             computerScore++;
-        } else if(computer == 'scissors') {
-            console.log("You won! Rock beats scissors.");
+        } else if(computer == scrs) {
+            message.appendChild("You won! Rock beats scissors.");
             userScore++
         }
-    } else if(player == 'paper') {
-        if(computer == 'rock') {
-            console.log("You won! Paper beats rock.");
+    } else if(player == paper) {
+        if(computer == rock) {
+            message.appendChild("You won! Paper beats rock.");
             userScore++;
-        } else if (computer == "scissors") {
-            console.log("You lose! Scissors beats paper.");
+        } else if (computer == scrs) {
+            message.appendChild("You lose! Scissors beats paper.");
             computerScore++;
         }
-    } else if (player == 'scissors') {
-        if(computer == 'rock') {
-            console.log("You lose! Rock beats Scissors");
+    } else if (player == scrs) {
+        if(computer == rock) {
+            message.appendChild("You lose! Rock beats Scissors");
             computerScore++;
-        } else if (computer == "paper") {
-            console.log("You won! Scissors beats paper.")
+        } else if (computer == paper) {
+            message.appendChild("You won! Scissors beats paper.")
             userScore++;
         }
     } 
 }
 
 function playGame() {
-    for(let i = 0; i < 5; i++){
-        const playerWeapon = prompt('Choose your weapon', 'Rock, Paper or Scissors').toLowerCase();
+  
+  let message2 = document.getElementById("message-2");
+  let weapons = [rock, paper, scrs];
+  
+    for(let i = 0; i < 3; i++){
+        const playerWeapon = weapons;
         const computerWeapon = getComputerChoice();
         game(playerWeapon, computerWeapon);
     }
-    console.log("The score is: User " + userScore + " Computer: " + computerScore + " Ties: " + tieCount);
+    message2.innerText = "The score is: User " + userScore + " Computer: " + computerScore + " Ties: " + tieCount;
     if(userScore > computerScore) {
-        console.log("%cYou are the fucking master of this game!", "background-color: green");
+        message2.innerText = "You are the fucking master of this game!";
     } else if(computerScore > userScore) {
-        console.log("%cYou suck! HAHAHAHAHAH", "background-color: red");
+        message2.innerText = "You suck! HAHAHAHAHAH";
     } else {
-        console.log("%cBoth of you suck!", "background-color: blue");
+        message2.innerText = "Both of you suck!";
+    } else {
+      message2.innerText = none;
     }
 }
 
 
 playGame();
-
