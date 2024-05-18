f7dcf19daeaf0cf98c08c2df48a6b8a1f42bfe1b
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-05-18 07:58:53 -0300

    fixed some bugs

--- script.js
+++ script.js
@@ -4,68 +4,67 @@ let computerScore = 0;
 
 const paper = 'paper';
 const rock = 'rock';
-const scrs = 'scissors';
+const scissors = 'scissors';
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 9) + 1;
-(computerChoice >= 1 && computerChoice <= 3) ? computerChoice = rock : (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = paper : computerChoice = scrs;
+    return (computerChoice >= 1 && computerChoice <= 3) ? rock :
+           (computerChoice >= 4 && computerChoice <= 6) ? paper :
+           scissors;
 }
 
 let game = (player, computer) => {
-  
-const message = document.getElementById("message");
-  
-    if(player == computer) {
-        message.appendChild("It's a tie!");
+    const message = document.getElementById("message");
+
+    if (player === computer) {
+        message.append("It's a tie!");
         tieCount++;
-    } else if(player == rock) {
-        if(computer == paper){
-            message.appendChild("You lose! Paper beats rock.");
+    } else if (player === rock) {
+        if (computer === paper) {
+            message.append("You lose! Paper beats rock.");
             computerScore++;
-        } else if(computer == scrs) {
-            message.appendChild("You won! Rock beats scissors.");
-            userScore++
-        }
-    } else if(player == paper) {
-        if(computer == rock) {
-            message.appendChild("You won! Paper beats rock.");
-            userScore++;
-        } else if (computer == scrs) {
-            message.appendChild("You lose! Scissors beats paper.");
-            computerScore++;
-        }
-    } else if (player == scrs) {
-        if(computer == rock) {
-            message.appendChild("You lose! Rock beats Scissors");
-            computerScore++;
-        } else if (computer == paper) {
-            message.appendChild("You won! Scissors beats paper.")
+        } else if (computer === scissors) {
+            message.append("You won! Rock beats scissors.");
             userScore++;
         }
-    } 
+    } else if (player === paper) {
+        if (computer === rock) {
+            message.append("You won! Paper beats rock.");
+            userScore++;
+        } else if (computer === scissors) {
+            message.append("You lose! Scissors beats paper.");
+            computerScore++;
+        }
+    } else if (player === scissors) {
+        if (computer === rock) {
+            message.append("You lose! Rock beats Scissors.");
+            computerScore++;
+        } else if (computer === paper) {
+            message.append("You won! Scissors beats paper.");
+            userScore++;
+        }
+    }
 }
 
 function playGame() {
-  
-  let message2 = document.getElementById("message-2");
-  let weapons = [rock, paper, scrs];
-  
-    for(let i = 0; i < 3; i++){
-        const playerWeapon = weapons;
+    let message2 = document.getElementById("message-2");
+    let weapons = [rock, paper, scissors];
+
+    for (let i = 0; i < 3; i++) {
+        const playerWeapon = weapons[i];
         const computerWeapon = getComputerChoice();
         game(playerWeapon, computerWeapon);
     }
-    message2.innerText = "The score is: User " + userScore + " Computer: " + computerScore + " Ties: " + tieCount;
-    if(userScore > computerScore) {
-        message2.innerText = "You are the fucking master of this game!";
-    } else if(computerScore > userScore) {
-        message2.innerText = "You suck! HAHAHAHAHAH";
+
+    message2.innerText = `The score is: User ${userScore} Computer: ${computerScore} Ties: ${tieCount}`;
+    
+    if (userScore > computerScore) {
+        message2.innerText = "You are the master of this game!";
+    } else if (computerScore > userScore) {
+        message2.innerText = "You lose! Better luck next time.";
     } else {
-        message2.innerText = "Both of you suck!";
-    } else {
-      message2.innerText = none;
+        message2.innerText = "It's a draw!";
     }
 }
-
 
 playGame();
